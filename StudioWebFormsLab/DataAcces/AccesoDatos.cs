79d8c9abf91239c44929bca9941bd9ae3ca73bf2
Aaron <aaronbellotrovira@gmail.com>
2017-05-03 23:06:36 +0200

    Añadidas referencia de la capa de entidades al resto de capas.

--- StudioWebFormsLab/DataAcces/AccesoDatos.cs
+++ StudioWebFormsLab/DataAcces/AccesoDatos.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntitiesLayer;
 
 namespace DataAccesLayer
 {
+
+    //COMPONENTES DE ACCESO A DATOS
+
+    //Los Componentes de Acceso a Datos(CADs) encapsulan la tecnología de acceso a datos 
+    //y la BD al resto de la aplicación.
+
+    //Proporciona un interfaz sencillo que permite recuperar los datos de la BD y 
+    //salvar una enAdad de negocio en la BD.
+
     public class AccesoDatos
     {
-         int a { get; set; }
-         int b { get; set; }
+       
+
 
 
     }
